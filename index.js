ea35354fe2c3abe8b652e54c3eaa6f07be66bf2c
Pushpendra-117 <cdharmendrasiha@gmail.com>
2025-07-30 18:35:43 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+{
+  "name": "treding-app",
+  "version": "1.0.0",
+  "description": "This is my web app hosted on Render.",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js"
+  },
+  "dependencies": {
+    "express": "^4.18.2"
+  },
+  "engines": {
+    "node": "18.x"
+  }
+}
